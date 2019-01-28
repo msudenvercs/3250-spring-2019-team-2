1fad3dbea9e8c6dc1f035dcaf191eb749683d55d
Megan Horton <mhorto17@msudenver.edu>
2019-01-28 15:53:29 -0700

    fixed HelloWorld

--- jvpm/HelloWorld.py
+++ jvpm/HelloWorld.py
@@ -1,8 +1,8 @@
 def HelloWorld():
     # Make sure to indent with four spaces and not a tab
-    print("Hello Megan")
     print("Hello World")
     print("Hello Robb")
+    print("Hello Megan")
 
 if '__main__' == __name__:
     HelloWorld()
