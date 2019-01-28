c2877af929373e58ddbb20aa01bb10dfe6502106
Robbie Hill <rhill53@msudenver.edu>
2019-01-28 07:23:22 -0700

    Robb corrected error with test

--- jvpm/test/test_HelloWorld.py
+++ jvpm/test/test_HelloWorld.py
@@ -6,7 +6,8 @@ from unittest.mock import Mock, call
 class TestHelloWorld(unittest.TestCase):
     def test_HelloWorld(self):
         sys.stdout = unittest.mock.Mock()
-        jvpm.HelloWorld.HelloWorld()
+        #jvpm.HelloWorld.HelloWorld()  #replaced by line below
+        HelloWorld.HelloWorld()
         sys.stdout.assert_has_calls(
             [call.write('Hello World'), call.write('\n')]
             [call.write('Hello Robb'), call.wrine('\n')]
