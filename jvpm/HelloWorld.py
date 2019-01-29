790da6c3f7ac8e20e7203a4d8f18e64bb9238607
Justin Fulner <jfulner@msudenver.edu>
2019-01-28 20:15:26 -0700

    updated HelloWorld.py and test_HelloWorld.py
    moved my name to suit the order better, also resolved an import issue within the test module.
    persiting error: the test module: test_HelloWorld is failing to import

--- jvpm/HelloWorld.py
+++ jvpm/HelloWorld.py
@@ -2,8 +2,8 @@ def HelloWorld():
     # Make sure to indent with four spaces and not a tab
     print("Hello World")
     print("Hello Robb")
-	print("Hello Justin")
     print("Hello Megan")
+    print("Hello Justin")
 
 if '__main__' == __name__:
     HelloWorld()
--- jvpm/test/test_HelloWorld.py
+++ jvpm/test/test_HelloWorld.py
@@ -1,16 +1,16 @@
 import unittest
 import sys
-import jvpm.HelloWorld
+# import jvpm.HelloWorld
+from ...jvpm.HelloWorld import HelloWorld
 from unittest.mock import Mock, call
 
 class TestHelloWorld(unittest.TestCase):
     def test_HelloWorld(self):
         sys.stdout = unittest.mock.Mock()
-        jvpm.HelloWorld.HelloWorld()
+        HelloWorld.HelloWorld()
         sys.stdout.assert_has_calls(
             [call.write('Hello World'), call.write('\n'),
-			call.write('Hello Robb'), call.write('\n'),
-			call.write('Hello Justin'), call.write('\n'),
-            call.write('Hello Megan'), call.write('\n')]
-
+             call.write('Hello Robb'), call.write('\n'),
+             call.write('Hello Megan'), call.write('\n'),
+             call.write('Hello Justin'), call.write('\n')]
         )
