dfd2256326c2f8025d3dac882590417edd8efd80
Justin Fulner <jfulner@msudenver.edu>
2019-01-29 01:17:09 -0700

    Update test_HelloWorld.py
    chanded the relative import back to an absolute import
    Persisting error: the jvpm folder is not being recognized as a module which is interfering with importing the test module

--- jvpm/test/test_HelloWorld.py
+++ jvpm/test/test_HelloWorld.py
@@ -1,13 +1,12 @@
 import unittest
 import sys
-# import jvpm.HelloWorld
-from ...jvpm.HelloWorld import HelloWorld
+import jvpm.HelloWorld
 from unittest.mock import Mock, call
 
 class TestHelloWorld(unittest.TestCase):
     def test_HelloWorld(self):
         sys.stdout = unittest.mock.Mock()
-        HelloWorld.HelloWorld()
+        jvpm.HelloWorld.HelloWorld()
         sys.stdout.assert_has_calls(
             [call.write('Hello World'), call.write('\n'),
              call.write('Hello Robb'), call.write('\n'),
